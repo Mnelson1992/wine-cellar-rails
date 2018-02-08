ef096a9a4e1a559ccfb9e244bd90c4a8ece25802
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-07 21:14:50 -0500

    Added a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'welcome#root'
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   post '/signout', to: 'sessions#destroy'
-  
+
   resources :users
 
   resources :bottles
