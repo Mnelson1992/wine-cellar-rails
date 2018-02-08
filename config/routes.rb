d965cadc54f2acdb28599fc0fa9d1b77f19ba1fd
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-07 20:57:32 -0500

    Added session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  post '/signout', to: 'sessions#destroy'
+  
   resources :users
 
   resources :bottles
