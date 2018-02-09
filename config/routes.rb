6a632bee88a188c4801e37f0594b63a9ed20de8f
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-02-08 21:02:25 -0500

    Finalized devise set up and changed the routes in the home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#home'
 
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
-  post '/signout', to: 'sessions#destroy'
-
 
   resources :bottles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
